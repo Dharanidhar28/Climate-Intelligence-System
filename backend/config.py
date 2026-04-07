8dc567b7880d9b8ef2aba0fd0ff976def4b2db4c
Dharanidhar28 <dharanidharkotha@gmail.com>
2026-04-07 16:54:35 +0530

    changed postgres database url

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv("database_url", "postgresql://postgres:12345678@localhost/climate_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://climate_db_fq5i_user:JMLeVDxpCcE91rA8cuLuDehDKj6zsEL8@dpg-d7aefjfpm1nc73fasfa0-a/climate_db_fq5i")
